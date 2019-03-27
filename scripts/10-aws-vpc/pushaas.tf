52ffb8d451063fdc358a9daf31b7616d54ea4bc6
Rafael Eyng <rafaeleyng@gmail.com>
2019-03-27 02:16:56 -0300

    open tsuru dashboard

--- scripts/10-aws-vpc/pushaas.tf
+++ scripts/10-aws-vpc/pushaas.tf
@@ -17,6 +17,13 @@ resource "aws_default_security_group" "default" {
     to_port   = 0
   }
 
+  ingress {
+    protocol  = "tcp"
+    from_port = 80
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
